85648ce0f9399038f8889fc8d6f2fe9eebaf0228
Eddie Simons <edmundsimons69@gmail.com>
2026-02-15 22:52:21 +0200

    Initial Commit

---
+++ PitBoy Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PitBoy Watch App
+//
+//  Created by Edmund Simons on 2026/02/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PitBoy Watch App/PitBoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitBoyApp.swift
+//  PitBoy Watch App
+//
+//  Created by Edmund Simons on 2026/02/15.
+//
+
+import SwiftUI
+
+@main
+struct PitBoy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
